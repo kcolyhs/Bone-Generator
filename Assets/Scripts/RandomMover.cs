c25024a18ced32cbe8127ef292a53ec1e41182c3
Bailey Chen <bchen919@gmail.com>
2018-04-19 20:22:56 -0400

    initial commit 2

---
+++ Assets/Scripts/RandomMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMover : MonoBehaviour {
+    HingeJoint2D hinge = null;
+    // Use this for initialization
+    void Start () {
+     hinge = gameObject.GetComponent(typeof(HingeJoint2D)) as HingeJoint2D;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
